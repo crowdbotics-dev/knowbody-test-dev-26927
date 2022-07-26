6512bcae3bbdc5f3cf4ad40226540c885178dab2
Crowdbotics <engineering@crowdbotics.com>
2022-07-26 22:10:40 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/knowbody_test_dev_26927/MainActivity.java
@@ -0,0 +1,15 @@
+package com.knowbody_test_dev_26927;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "knowbody_test_dev_26927";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "knowbody_test_dev_26927",
+  url: "https://knowbody_test_dev_26927.botics.co",
+  api: "https://knowbody_test_dev_26927.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
